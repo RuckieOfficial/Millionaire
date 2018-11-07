bd6be4520e1c66ba3bfa0ae284bc193a4e807101
Lukáš Rücker <ruckelu16@sps-prosek.cz>
2018-11-07 16:04:00 +0100

    init

--- Millionire/Highscore.xaml.cs
+++ Millionire/Highscore.xaml.cs
@@ -44,18 +44,24 @@ namespace Millionire {
             main.Show();
         }
 
-        void score() {
-            var serazene = highscore.OrderBy(n => n.score).ToList();
-            jmeno1.Content = serazene[1].nick + " " + serazene[1].score;
-            jmeno2.Content = serazene[2].nick + " " + serazene[2].score;
-            jmeno3.Content = serazene[3].nick + " " + serazene[3].score;
-            jmeno4.Content = serazene[4].nick + " " + serazene[4].score;
-            jmeno5.Content = serazene[5].nick + " " + serazene[5].score;
-            jmeno6.Content = serazene[6].nick + " " + serazene[6].score;
-            jmeno7.Content = serazene[7].nick + " " + serazene[7].score;
-            jmeno8.Content = serazene[8].nick + " " + serazene[8].score;
-            jmeno9.Content = serazene[9].nick + " " + serazene[9].score;
-            jmeno10.Content = serazene[10].nick + " " + serazene[10].score;
+        void domex(object sender, RoutedEventArgs e) {
+            MainWindow main = new MainWindow();
+            this.Close();
+            main.Show();
+        }
+
+            void score() {
+            var serazene = highscore.OrderBy(n => n.score).Reverse().ToList();
+            jmeno1.Content = serazene[0].nick + " " + serazene[0].score;
+            jmeno2.Content = serazene[1].nick + " " + serazene[1].score;
+            jmeno3.Content = serazene[2].nick + " " + serazene[2].score;
+            jmeno4.Content = serazene[3].nick + " " + serazene[3].score;
+            jmeno5.Content = serazene[4].nick + " " + serazene[4].score;
+            jmeno6.Content = serazene[5].nick + " " + serazene[5].score;
+            jmeno7.Content = serazene[6].nick + " " + serazene[6].score;
+            jmeno8.Content = serazene[7].nick + " " + serazene[7].score;
+            jmeno9.Content = serazene[8].nick + " " + serazene[8].score;
+            jmeno10.Content = serazene[9].nick + " " + serazene[9].score;
         }
         void reset() {
            List<HighScore_items> highscore = new List<HighScore_items>();
